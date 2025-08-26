77e237b5e4b6113fb44cbbce21c1f37d906335dd
Shafawra <shafaaura35@gmail.com>
2025-08-26 22:47:33 +0800

    melakukan perubahan dan penambahan pada file hmtl, css, js

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ document.querySelector(".next").addEventListener("click", () => {
 document.querySelector(".prev").addEventListener("click", () => {
     movieRow.scrollBy({ left: -300, behavior: "smooth"});
 });
+
+
+const items = document.querySelectorAll(".accItem");
+
+items.forEach(item => {
+    const accHeader = item.querySelector(".accHeader");
+    const accContent = item.querySelector(".accContent");
+    const icon = item.querySelector(".icon");
+
+    accHeader.addEventListener("click", () => {
+        items.forEach(i => {
+            i.querySelector(".accContent").style.maxHeight = null;
+            i.querySelector(".icon").style.transform = "rotate(0deg)";
+        });
+
+        if (!accContent.style.maxHeight) {
+            accContent.style.maxHeight = accContent.scrollHeight + "px";
+            icon.style.transform = "rotate(45deg)";
+        } else {
+            accContent.style.maxHeight = null;
+            icon.style.transform = "rotate(0deg)";
+        }
+    });
+});
